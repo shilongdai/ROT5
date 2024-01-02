882875da9c5443f601dbd8d2d770512757260fc9
Shilong Dai <sdai@viperfish.net>
2024-01-02 10:39:39 -0500

    Adjust hyper-params

--- migrate_rope_gattention.sh
+++ migrate_rope_gattention.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 deepspeed migrate_mlm.py --output_dir="./rebert_roberta" --final_output_dir "./rebert_roberta_best" \
-  --evaluation_strategy "steps" --per_device_train_batch_size 64 --per_device_eval_batch_size 64 \
-  --gradient_accumulation_steps 64 --max_grad_norm 0.5 \
+  --evaluation_strategy "steps" --per_device_train_batch_size 60 --per_device_eval_batch_size 60 \
+  --gradient_accumulation_steps 68 --max_grad_norm 0.5 \
   --learning_rate 0.001 --num_train_epochs 2 --weight_decay 0.01 --warmup_ratio 0.5 --lr_scheduler_type "linear" \
   --logging_dir "tb_rebert_roberta" --logging_steps 50 \
   --save_steps 50 --save_strategy "steps" --save_total_limit 10 --load_best_model_at_end True \
