e81d45491db887fffecf3759919ddcbd664564c2
Shilong Dai <shilongdai@gmail.com>
2024-01-04 17:03:17 -0500

    Adds manual batch size schedule

--- pretrain_mlm_minipile.sh
+++ pretrain_mlm_minipile.sh
@@ -1,10 +1,55 @@
 #!/bin/bash
 
-deepspeed train_mlm.py --output_dir="./rebert_minipile" --final_output_dir "./rebert_minipile_best" \
-  --evaluation_strategy "steps" --per_device_train_batch_size 200 --per_device_eval_batch_size 200 \
-  --gradient_accumulation_steps 2 --max_grad_norm 0.5 \
-  --learning_rate 0.001 --num_train_epochs 4 --weight_decay 0.01 --warmup_ratio 0.02 --lr_scheduler_type "linear" \
-  --logging_dir "tb_rebert_minipile" --logging_steps 50 \
-  --save_steps 50 --save_strategy "steps" --save_total_limit 10 --load_best_model_at_end True \
+deepspeed train_mlm.py --output_dir="./rebert_minipile_128" --final_output_dir "./rebert_minipile_best_128" \
+  --evaluation_strategy "steps" --per_device_train_batch_size 32 --per_device_eval_batch_size 200 \
+  --gradient_accumulation_steps 1 \
+  --learning_rate 0.001 --num_train_epochs 1 --weight_decay 0.01 --warmup_ratio 0.02 --lr_scheduler_type "linear" \
+  --logging_dir "tb_rebert_minipile" --logging_steps 500 \
+  --save_steps 500 --save_strategy "steps" --save_total_limit 10 --load_best_model_at_end False \
   --bf16 True --gradient_checkpointing True --deepspeed "./deepspeed/deepspeed_2.json" \
   --dataset_path "data/minipile" --eval_name "validation" --tokenizer_name "mistralai/Mistral-7B-Instruct-v0.2"
+
+deepspeed train_mlm.py --output_dir="./rebert_minipile_256" --final_output_dir "./rebert_minipile_best_256" \
+  --evaluation_strategy "steps" --per_device_train_batch_size 64 --per_device_eval_batch_size 200 \
+  --gradient_accumulation_steps 1 \
+  --learning_rate 0.0005 --num_train_epochs 1 --weight_decay 0.01 --warmup_ratio 0.02 --lr_scheduler_type "linear" \
+  --logging_dir "tb_rebert_minipile" --logging_steps 250 \
+  --save_steps 250 --save_strategy "steps" --save_total_limit 10 --load_best_model_at_end False \
+  --bf16 True --gradient_checkpointing True --deepspeed "./deepspeed/deepspeed_2.json" \
+  --dataset_path "data/minipile" --eval_name "validation" --model_path "./rebert_minipile_best_128"
+
+deepspeed train_mlm.py --output_dir="./rebert_minipile_512" --final_output_dir "./rebert_minipile_best_512" \
+  --evaluation_strategy "steps" --per_device_train_batch_size 128 --per_device_eval_batch_size 200 \
+  --gradient_accumulation_steps 1 \
+  --learning_rate 0.0001 --num_train_epochs 1 --weight_decay 0.01 --warmup_ratio 0.02 --lr_scheduler_type "linear" \
+  --logging_dir "tb_rebert_minipile" --logging_steps 125 \
+  --save_steps 125 --save_strategy "steps" --save_total_limit 10 --load_best_model_at_end False \
+  --bf16 True --gradient_checkpointing True --deepspeed "./deepspeed/deepspeed_2.json" \
+  --dataset_path "data/minipile" --eval_name "validation" --model_path "./rebert_minipile_best_256"
+
+deepspeed train_mlm.py --output_dir="./rebert_minipile_800" --final_output_dir "./rebert_minipile_best_800" \
+  --evaluation_strategy "steps" --per_device_train_batch_size 200 --per_device_eval_batch_size 200 \
+  --gradient_accumulation_steps 1 \
+  --learning_rate 0.0001 --num_train_epochs 1 --weight_decay 0.01 --warmup_ratio 0.02 --lr_scheduler_type "linear" \
+  --logging_dir "tb_rebert_minipile" --logging_steps 100 \
+  --save_steps 100 --save_strategy "steps" --save_total_limit 10 --load_best_model_at_end False \
+  --bf16 True --gradient_checkpointing True --deepspeed "./deepspeed/deepspeed_2.json" \
+  --dataset_path "data/minipile" --eval_name "validation" --model_path "./rebert_minipile_best_512"
+
+deepspeed train_mlm.py --output_dir="./rebert_minipile_1600" --final_output_dir "./rebert_minipile_best_1600" \
+  --evaluation_strategy "steps" --per_device_train_batch_size 200 --per_device_eval_batch_size 200 \
+  --gradient_accumulation_steps 2 \
+  --learning_rate 0.00005 --num_train_epochs 1 --weight_decay 0.01 --warmup_ratio 0.02 --lr_scheduler_type "linear" \
+  --logging_dir "tb_rebert_minipile" --logging_steps 100 \
+  --save_steps 100 --save_strategy "steps" --save_total_limit 10 --load_best_model_at_end False \
+  --bf16 True --gradient_checkpointing True --deepspeed "./deepspeed/deepspeed_2.json" \
+  --dataset_path "data/minipile" --eval_name "validation" --model_path "./rebert_minipile_best_800"
+
+deepspeed train_mlm.py --output_dir="./rebert_minipile_2400" --final_output_dir "./rebert_minipile_best_2400" \
+  --evaluation_strategy "steps" --per_device_train_batch_size 200 --per_device_eval_batch_size 200 \
+  --gradient_accumulation_steps 3 \
+  --learning_rate 0.00005 --num_train_epochs 1 --weight_decay 0.01 --warmup_ratio 0.02 --lr_scheduler_type "linear" \
+  --logging_dir "tb_rebert_minipile" --logging_steps 50 \
+  --save_steps 50 --save_strategy "steps" --save_total_limit 10 --load_best_model_at_end False \
+  --bf16 True --gradient_checkpointing True --deepspeed "./deepspeed/deepspeed_2.json" \
+  --dataset_path "data/minipile" --eval_name "validation" --model_path "./rebert_minipile_best_1600"
--- train_mlm.py
+++ train_mlm.py
@@ -6,7 +6,7 @@ import numpy as np
 import torch
 from datasets import load_from_disk
 from transformers import AutoTokenizer, DataCollatorForLanguageModeling, TrainingArguments, \
-    Trainer, HfArgumentParser
+    Trainer, HfArgumentParser, AutoModelForMaskedLM
 
 from rebert.model import (ReBertConfig, ReBertForMaskedLM)
 
@@ -14,6 +14,7 @@ from rebert.model import (ReBertConfig, ReBertForMaskedLM)
 @dataclass
 class ScriptArguments:
     tokenizer_name: Optional[str] = field(default="mistralai/Mistral-7B-Instruct-v0.2")
+    model_path: Optional[str] = field(default=None)
     dataset_path: Optional[str] = field(default="./data/mlm")
     train_name: Optional[str] = field(default="train")
     eval_name: Optional[str] = field(default="eval")
@@ -33,33 +34,37 @@ if __name__ == "__main__":
     np.random.seed(train_args.seed)
     torch.manual_seed(train_args.seed)
 
-    tokenizer = AutoTokenizer.from_pretrained(script_args.tokenizer_name)
-    if not tokenizer.pad_token_id:
-        tokenizer.add_special_tokens({"pad_token": "[PAD]"})
-        print(f"Added Pad Token: {tokenizer.pad_token_id}")
-    if not tokenizer.mask_token_id:
-        tokenizer.add_special_tokens({"mask_token": "[MASK]"})
-        print(f"Added Mask Token: {tokenizer.mask_token_id}")
+    if not script_args.model_path:
+        tokenizer = AutoTokenizer.from_pretrained(script_args.tokenizer_name)
+        if not tokenizer.pad_token_id:
+            tokenizer.add_special_tokens({"pad_token": "[PAD]"})
+            print(f"Added Pad Token: {tokenizer.pad_token_id}")
+        if not tokenizer.mask_token_id:
+            tokenizer.add_special_tokens({"mask_token": "[MASK]"})
+            print(f"Added Mask Token: {tokenizer.mask_token_id}")
 
-    print(f"Final Vocab Size: {tokenizer.vocab_size}")
-    max_length = script_args.model_max_length
-    config = ReBertConfig(
-        pad_token_id=tokenizer.pad_token_id,
-        bos_token_id=tokenizer.bos_token_id,
-        eos_token_id=tokenizer.eos_token_id,
-        vocab_size=len(tokenizer),
-        hidden_size=768,
-        num_hidden_layers=12,
-        num_attention_heads=12,
-        intermediate_size=3072,
-        hidden_act="gelu",
-        hidden_dropout_prob=0.1,
-        attention_probs_dropout_prob=0.1,
-        layer_norm_eps=1e-12,
-        classifier_dropout=0.1,
-        max_length=max_length
-    )
-    rope_bert = ReBertForMaskedLM(config)
+        print(f"Final Vocab Size: {tokenizer.vocab_size}")
+        max_length = script_args.model_max_length
+        config = ReBertConfig(
+            pad_token_id=tokenizer.pad_token_id,
+            bos_token_id=tokenizer.bos_token_id,
+            eos_token_id=tokenizer.eos_token_id,
+            vocab_size=len(tokenizer),
+            hidden_size=768,
+            num_hidden_layers=12,
+            num_attention_heads=12,
+            intermediate_size=3072,
+            hidden_act="gelu",
+            hidden_dropout_prob=0.1,
+            attention_probs_dropout_prob=0.1,
+            layer_norm_eps=1e-12,
+            classifier_dropout=0.1,
+            max_length=max_length
+        )
+        rope_bert = ReBertForMaskedLM(config)
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(script_args.model_path)
+        rope_bert = AutoModelForMaskedLM.from_pretrained(script_args.model_path)
     data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probability=script_args.mlm_prob, mlm=True)
     print(f"Loading data from: {script_args.dataset_path}")
     ds = load_from_disk(script_args.dataset_path)
