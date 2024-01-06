a20dd7ce746c85e2d9358596b086e6881cad3405
Shilong Dai <sdai@viperfish.net>
2024-01-06 17:44:03 -0500

    Updated params for training from scratch

--- migrate_gattention.sh
+++ migrate_gattention.sh
@@ -3,7 +3,7 @@
 deepspeed migrate_mlm.py --output_dir="./rebert_g4" --final_output_dir "./rebert_g4_best" \
   --evaluation_strategy "steps" --per_device_train_batch_size 128 --per_device_eval_batch_size 128 \
   --gradient_accumulation_steps 2 \
-  --learning_rate 0.0007 --max_steps 6000 --weight_decay 0.01 --warmup_ratio 0.01 --lr_scheduler_type "linear" \
+  --learning_rate 0.0007 --max_steps 8000 --weight_decay 0.01 --warmup_ratio 0.01 --lr_scheduler_type "linear" \
   --logging_dir "tb_rebert_g4" --logging_steps 50 \
   --save_steps 50 --save_strategy "steps" --save_total_limit 10 --load_best_model_at_end True \
   --bf16 True --gradient_checkpointing True --deepspeed "./deepspeed/deepspeed_2.json" \
--- pretrain_mlm_minipile.sh
+++ pretrain_mlm_minipile.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
  deepspeed train_mlm.py --output_dir="./rebert_minipile" --final_output_dir "./rebert_minipile_best" \
-   --evaluation_strategy "steps" --per_device_train_batch_size 128 --per_device_eval_batch_size 128 \
-   --gradient_accumulation_steps 2 \
-   --learning_rate 0.0002 --max_steps 6000 --weight_decay 0.01 --warmup_ratio 0.06 --lr_scheduler_type "linear" \
-   --logging_dir "tb_rebert_minipile" --logging_steps 50 \
-   --save_steps 50 --save_strategy "steps" --save_total_limit 10 --load_best_model_at_end True \
+   --evaluation_strategy "steps" --per_device_train_batch_size 512 --per_device_eval_batch_size 512 \
+   --gradient_accumulation_steps 4 \
+   --learning_rate 0.001 --max_steps 25000 --weight_decay 0.01 --warmup_ratio 0.02 --lr_scheduler_type "linear" \
+   --logging_dir "tb_rebert_minipile" --logging_steps 100 \
+   --save_steps 100 --save_strategy "steps" --save_total_limit 10 --load_best_model_at_end True \
    --bf16 True --gradient_checkpointing True --deepspeed "./deepspeed/deepspeed_2.json" \
-   --dataset_path "data/minipile_roberta" --eval_name "validation" --model_path "./rebert_roberta_best"
-
-deepspeed train_mlm.py --output_dir="./rebert_minipile_2" --final_output_dir "./rebert_minipile_2best" \
-  --evaluation_strategy "steps" --per_device_train_batch_size 128 --per_device_eval_batch_size 128 \
-  --gradient_accumulation_steps 2 \
-  --learning_rate 0.00002 --max_steps 2600 --weight_decay 0.01 --lr_scheduler_type "linear" \
-  --logging_dir "tb_rebert_minipile2" --logging_steps 50 \
-  --save_steps 50 --save_strategy "steps" --save_total_limit 10 --load_best_model_at_end True \
-  --bf16 True --gradient_checkpointing True --deepspeed "./deepspeed/deepspeed_2.json" \
-  --dataset_path "data/minipile_roberta" --eval_name "validation" --model_path "./rebert_minipile_best"
+   --dataset_path "data/minipile_mistral" --eval_name "validation" --model_path "./rebert_best"
