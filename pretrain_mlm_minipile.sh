873c83cca86943798dbf0d05edb42c86ac2283eb
Shilong Dai <shilongdai@gmail.com>
2024-01-07 00:32:37 +0000

    Pretraining params set

--- pretrain_mlm_minipile.sh
+++ pretrain_mlm_minipile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
  deepspeed train_mlm.py --output_dir="./rebert_minipile" --final_output_dir "./rebert_minipile_best" \
-   --evaluation_strategy "steps" --per_device_train_batch_size 170 --per_device_eval_batch_size 170 \
-   --gradient_accumulation_steps 6 \
-   --learning_rate 0.001 --max_steps 25000 --weight_decay 0.01 --warmup_ratio 0.02 --lr_scheduler_type "linear" \
+   --evaluation_strategy "steps" --per_device_train_batch_size 190 --per_device_eval_batch_size 190 \
+   --gradient_accumulation_steps 1 \
+   --learning_rate 0.001 --max_steps 23000 --weight_decay 0.01 --warmup_ratio 0.02 --lr_scheduler_type "linear" \
    --logging_dir "tb_rebert_minipile" --logging_steps 100 \
    --save_steps 100 --save_strategy "steps" --save_total_limit 10 --load_best_model_at_end True \
    --bf16 True --gradient_checkpointing True --deepspeed "./deepspeed/deepspeed_2.json" \
