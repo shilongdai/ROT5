0b9ca1c767c5082e67563691a7e111b67a9ac621
Shilong Dai <sdai@viperfish.net>
2024-01-09 18:48:21 -0500

    Adjust hyper-params

--- pretrain_ul2_tinystories.sh
+++ pretrain_ul2_tinystories.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
  deepspeed train_ul2.py --output_dir="./rebert_tinystories" --final_output_dir "./rebert_tinystories_best" \
-   --evaluation_strategy "steps" --per_device_train_batch_size 64 --per_device_eval_batch_size 64 \
-   --gradient_accumulation_steps 2 \
-   --learning_rate 0.0001 --max_steps 16000 --weight_decay 0.01 --warmup_ratio 0.01 --lr_scheduler_type "linear" \
-   --logging_dir "tb_rebert_tinystories" --logging_steps 100 \
+   --evaluation_strategy "steps" --per_device_train_batch_size 128 --per_device_eval_batch_size 128 \
+   --gradient_accumulation_steps 1 \
+   --learning_rate 0.0001 --num_train_epochs 1 --weight_decay 0.01 --warmup_ratio 0.01 --lr_scheduler_type "linear" \
+   --logging_dir "tb_rebert_tinystories" --logging_steps 100 --generation_max_length 512 \
    --save_steps 100 --save_strategy "steps" --save_total_limit 100 --load_best_model_at_end True \
    --bf16 True --gradient_checkpointing True --deepspeed "./deepspeed/deepspeed_2.json" \
    --dataset_path "data/tinystories_mistral" --eval_name "validation" \
