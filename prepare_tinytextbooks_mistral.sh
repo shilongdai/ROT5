ba5c2e77e707fb164d7faf5aa0f5aca89d85aef8
Shilong Dai <sdai@viperfish.net>
2024-01-11 10:44:19 -0500

    Fix typo

--- prepare_tinytextbooks_mistral.sh
+++ prepare_tinytextbooks_mistral.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 python prepare_pretrain_data.py --dataset_path "nampdn-ai/tiny-textbooks" \
-  --tokenizer_name "./rebert_tinystories_best" --text_field "textbook " --model_max_length 512 \
+  --tokenizer_name "./rebert_tinystories_best" --text_field "textbook" --model_max_length 512 \
   --final_output_dir "./data/tinytextbooks_mistral" --dataset_split "train" --pack False
