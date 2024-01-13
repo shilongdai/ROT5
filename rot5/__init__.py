5b7cc98243f3216883489921b9982ea6a1cc61dd
Shilong Dai <sdai@viperfish.net>
2024-01-13 13:22:26 -0500

    Renamed model type

--- rot5/__init__.py
+++ rot5/__init__.py
@@ -2,6 +2,6 @@ from transformers import AutoConfig, AutoModel, AutoModelForSeq2SeqLM
 
 from .rot5_model import ROT5Config, ROT5ForConditionalGeneration
 
-AutoConfig.register("rebert", ROT5Config)
+AutoConfig.register("rot5", ROT5Config)
 AutoModel.register(ROT5Config, ROT5ForConditionalGeneration)
 AutoModelForSeq2SeqLM.register(ROT5Config, ROT5ForConditionalGeneration)
