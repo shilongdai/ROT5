bcc0719889d8aeb6154a92da3f349d5666fb89fe
Shilong Dai <shilongdai@gmail.com>
2024-01-16 08:09:04 -0500

    Adds GQV for ROT5

--- pretrain_ul2_tinystories_gqa.sh
+++ pretrain_ul2_tinystories_gqa.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
- deepspeed train_ul2.py --output_dir="./rot5_tinystories" --final_output_dir "./rot5_tinystories_best" \
+ deepspeed train_ul2.py --output_dir="./grot5_tinystories" --final_output_dir "./grot5_tinystories_best" \
    --evaluation_strategy "steps" --per_device_train_batch_size 150 --per_device_eval_batch_size 150 \
    --gradient_accumulation_steps 1 \
    --learning_rate 0.0005 --num_train_epochs 1 --weight_decay 0.01 --warmup_ratio 0.01 --lr_scheduler_type "linear" \
-   --logging_dir "tb_rot5_tinystories" --logging_steps 100 --generation_max_length 512 \
+   --logging_dir "tb_grot5_tinystories" --logging_steps 100 --generation_max_length 512 \
    --save_steps 100 --save_strategy "steps" --save_total_limit 10 --load_best_model_at_end True \
    --bf16 True --gradient_checkpointing True --deepspeed "./deepspeed/deepspeed_2.json" \
    --dataset_path "data/tinystories_mistral" --eval_name "validation" \
