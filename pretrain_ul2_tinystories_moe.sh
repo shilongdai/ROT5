5fa25c4ead6538746010d9f8bb0d16c5fff13ca4
Shilong Dai <shilongdai@gmail.com>
2024-01-29 21:43:06 -0500

    reduces MOE save limits

--- pretrain_ul2_tinystories_moe.sh
+++ pretrain_ul2_tinystories_moe.sh
@@ -5,7 +5,7 @@
    --gradient_accumulation_steps 1 \
    --learning_rate 0.0005 --num_train_epochs 1 --weight_decay 0.01 --warmup_ratio 0.01 --lr_scheduler_type "linear" \
    --logging_dir "tb_mogrot5_tinystories" --logging_steps 100 --generation_max_length 512 \
-   --save_steps 100 --save_strategy "steps" --save_total_limit 10 --load_best_model_at_end True \
+   --save_steps 100 --save_strategy "steps" --save_total_limit 5 --load_best_model_at_end True \
    --bf16 True --gradient_checkpointing True --deepspeed "./deepspeed/deepspeed_2.json" \
    --dataset_path "data/tinystories_mistral" --eval_name "validation" \
    --tokenizer_name "mistralai/Mistral-7B-Instruct-v0.2" --kv_heads 4 --num_experts 8
