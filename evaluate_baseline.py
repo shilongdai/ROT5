ad3a049066f9d316dd64b23ad2193e8562e51093
Shilong Dai <shilongdai@gmail.com>
2024-02-03 22:36:14 -0500

    Adds full eval script

---
+++ evaluate_baseline.py
@@ -0,0 +1,54 @@
+import random
+from dataclasses import dataclass, field
+from typing import Optional, cast
+
+from datasets import load_from_disk
+from transformers import HfArgumentParser, PreTrainedTokenizer, AutoTokenizer, AutoModelForSeq2SeqLM, \
+    Seq2SeqTrainingArguments, Seq2SeqTrainer
+import numpy as np
+import torch
+
+from text_denoising import DataCollatorForUL2
+
+
+@dataclass
+class ScriptArguments:
+    deepspeed: Optional[str] = field(default=None)
+    model_path: Optional[str] = field(default=None)
+    dataset_path: Optional[str] = field(default="./data/mlm")
+    eval_name: Optional[str] = field(default="eval")
+    cache_dir: Optional[str] = field(default="./transformers_cache")
+    seed: Optional[int] = field(default=42)
+
+
+if __name__ == "__main__":
+    parser = HfArgumentParser([ScriptArguments])
+    script_args = parser.parse_args_into_dataclasses()[0]
+    script_args: ScriptArguments = cast(ScriptArguments, script_args)
+
+    random.seed(script_args.seed)
+    np.random.seed(script_args.seed)
+    torch.manual_seed(script_args.seed)
+
+    train_args = Seq2SeqTrainingArguments(output_dir="output", deepspeed=script_args.deepspeed)
+    tokenizer: PreTrainedTokenizer = AutoTokenizer.from_pretrained(script_args.model_path)
+    model = AutoModelForSeq2SeqLM.from_pretrained(script_args.model_path)
+
+
+    def sentinel_from_end(ids: np.ndarray, max_bound: int):
+        return max_bound - ids
+
+
+    data_collator = DataCollatorForUL2(tokenizer=tokenizer,
+                                       decoder_start_token_id=model.config.decoder_start_token_id,
+                                       sentinel_map=lambda x: sentinel_from_end(x, model.config.decoder_start_token_id))
+    print(f"Loading data from: {script_args.dataset_path}")
+    ds = load_from_disk(script_args.dataset_path)
+    eval_set = ds[script_args.eval_name]
+    trainer = Seq2SeqTrainer(
+        model=model,
+        args=train_args,
+        eval_dataset=eval_set,
+        data_collator=data_collator,
+    )
+    print(trainer.evaluate())
